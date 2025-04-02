d5bb9ecad4d47b84cdce1d7abb853796138be6cf
João Piologo <joaotpiologo@hotmail.com>
2025-04-01 22:10:30 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('CleanUp APIs')
+    .setDescription('API list and descriptions')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api-docs', app, document)
+
+  await app.listen(process.env.PORT ?? 3000)
+}
+bootstrap()
