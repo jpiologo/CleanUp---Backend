69a7edbe8069611c256ce9b06d3752467434dd08
João Piologo <joaotpiologo@hotmail.com>
2025-04-02 10:43:12 -0300

    Patch: Create Prisma Schemas

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  // Configuração do Swagger
+  // Configuração do Swagger API
   const config = new DocumentBuilder()
     .setTitle('CleanUp APIs')
     .setDescription('API list and descriptions')
