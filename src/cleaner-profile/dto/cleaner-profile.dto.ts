831b5c34e7d4caed39c24574d1a1c5cb949f2b6c
João Piologo <joaotpiologo@hotmail.com>
2025-04-12 19:50:28 -0300

    Patch: New methods on calculating distances between addresses

--- src/cleaner-profile/dto/cleaner-profile.dto.ts
+++ src/cleaner-profile/dto/cleaner-profile.dto.ts
@@ -14,6 +14,12 @@ export class CreateCleanerProfileDto {
 
 export class UpdateCleanerProfileDto extends PartialType(CreateCleanerProfileDto) {}
 
+export class GetNearbyCleanerProfilesDto {
+  @ApiProperty({ description: 'ID of the address to use for proximity filtering' })
+  @IsUUID()
+  addressId: string;
+}
+
 export class DisableCleanerProfileDto {
   @ApiProperty({ description: 'ID of the user to disable profile for' })
   @IsUUID()
